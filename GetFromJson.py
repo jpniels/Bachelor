1e9454279d4d9de3f92222a4366003da3313ad74
jpniels <jonasphillipn@gmail.com>
2018-03-19 12:35:45 +0100

    fixed getReadings in GetFromJson

--- GetFromJson.py
+++ GetFromJson.py
@@ -1,21 +1,30 @@
 import pandas as pd
+import json
 
 
 
 def read_file_path(path):
     with open(path, 'r'):
-            objects  = pd.read_json(path)
+            objects = pd.read_json(path)
             return objects
 
 data = read_file_path('ou44_gnd.json')
 #data['TimeInMiliseconds'] = pd.to_datetime(data['TimeInMiliseconds'], unit='ms')
-data = data.rename(columns={'TimeInMiliseconds': 'Date'})
-print(data)
+#data = data.rename(columns={'TimeInMiliseconds': 'Date'})
+
+
 
 def getReadings():
-    readings = data.where(data.Unit == 'degree celcius')
-    return readings.Reading
+    time = []
+    measurement = []
+    readings = data.Readings[2]
+    print(len(readings))
+    for i in range(0, len(readings)):
+        time.append( readings[i][0])
+        measurement.append(readings[i][1])
 
+    return time, measurement
+print(getReadings())
 def getTime():
     time = data.where(data.Unit == 'degree celcius')
     return time.Date  
