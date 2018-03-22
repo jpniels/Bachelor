5c50ec69ff8ded77937b358a630226d288159d2b
Sebzlol <pwnage@live.dk>
2018-03-22 11:03:13 +0100

    Cleaning Code

--- GetFromJson.py
+++ GetFromJson.py
@@ -9,9 +9,14 @@ def read_file_path(path):
 
 data = read_file_path('ou44_gnd.json')
 
+#Get a dataframe with timeintervals and mean readings for those
 def getDataframe():
-    return setIntervals()
-
+    readings = getReadings()
+    time = getTime()
+    df = pd.DataFrame({'timestamp':time.values, 'readings':readings.values})
+    df = df.groupby(pd.Grouper(key='timestamp', freq='12H'))['readings'].mean()
+    df = df.dropna()
+    return df
 
 #Get all the rooms from the data
 def getRooms():
@@ -36,27 +41,22 @@ def getTime():
     return time
 
 #Return dataframe with time and reading intervals
-def setIntervals():
-    readings = getReadings()
-    time = getTime()
-    
-    df = pd.DataFrame({'timestamp':time.values, 'readings':readings.values})
-    df = df.groupby(pd.Grouper(key='timestamp', freq='12H'))['readings'].mean() #Time intervals with readings mean value
-    
+def setReadingIntervals():
+    df = getDataframe()
     df = pd.cut(df, bins=[17,18,19,20,21,22,23,24,25,26,27,28], labels=['17-18','18-19','19-20','20-21','21-22','22-23','23-24','24-25','25-26','26-27','27-28'])
-    #Degrees interval
+    return df
+
+def getBooleanAssociationRules():
+    df = setReadingIntervals()
+    df = pd.get_dummies(df)
     return df
 
 #Set interpolation
 def createInterpolation(interval):
-    readings = getReadings()
-    time = getTime()
-    df = pd.DataFrame({'timestamp':time.values, 'readings':readings.values})
-    df = df.groupby(pd.Grouper(key='timestamp', freq='12H'))['readings'].mean()
-
+    df = getDataframe()
     timerange = pd.date_range(df.index[0], df.index[-1], freq=interval, normalize=True)
 
-    newdf = timerange.union(df.index)
-    newdf = df.reindex(newdf)
-    newdf = newdf.interpolate(method="time")
-    return newdf
+    dfWithIntervals = timerange.union(df.index)
+    dfWithIntervals = df.reindex(dfWithIntervals)
+    dfWithIntervals = dfWithIntervals.interpolate(method="time")
+    return dfWithIntervals
