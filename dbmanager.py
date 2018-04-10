d2c32652bc63eb20c94251f2a195612c0008731e
Sebzlol <pwnage@live.dk>
2018-04-10 12:13:53 +0200

    Cleaning repository

--- dbmanager.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb
-
-def dbConnect():
-    database = MySQLdb.connect( host = "localhost", user = "root", passwd = "", db = "bachelor")
-    cursor = database.cursor()
-
-def fetchUsername():
-    query ="""SELECT uname FROM users"""
